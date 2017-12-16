b71cb1e75f97e009a938371c880958dd0633458f
Jack Guldan <jackguldan@gmail.com>
2017-12-15 22:24:12 -0600

    added the demo code or proof of concept code for mixing RAzor and JavaScript

---
+++ NorthwindClass/Models/ChartData.cs
@@ -0,0 +1,14 @@
+﻿namespace Northwind.Models
+{
+    public class ChartData
+    {
+        public string company_name { get; set; }
+        public string present_worth { get; set; }
+
+        public ChartData(string name, string worth)
+        {
+            company_name = name;
+            present_worth = worth;
+        }
+    }
+}
